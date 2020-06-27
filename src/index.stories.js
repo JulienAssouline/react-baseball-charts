7824f9c70adb3f1cb3903238d7cb30443e1e289f
JulienAssouline <julien1993@hotmail.ca>
2020-06-27 13:32:51 -0400

    remove index.stories.js button code

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,15 +1,1 @@
-import React from "react";
-import { Button } from "@storybook/react/demo";
-import ButtonCool from "./components/Button";
 
-export default { title: "Button" };
-
-export const CoolButton = ({ text }) => <ButtonCool text={text} />;
-
-export const withEmoji = () => (
-  <Button>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
